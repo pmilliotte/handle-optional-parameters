7c906c3c174b9e077c4a874a49c529df0bf66ac4
Pierre Milliotte <pierre.milliotte.ext@axaclimate.com>
2023-05-05 17:47:09 +0200

    feat: setup linter configuration

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,18 @@
 // import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface HandleOptionalParametersProps {
   // Define construct properties here
 }
 
 export class HandleOptionalParameters extends Construct {
-
-  constructor(scope: Construct, id: string, props: HandleOptionalParametersProps = {}) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: HandleOptionalParametersProps = {}
+  ) {
     super(scope, id);
 
     // Define construct contents here
